46e6e52c3fe0de1bb8307f25e8f9efa902cd1ee0
MuhammmadFahry <fahriyansahm@gmail.com>
2024-08-21 16:23:29 +0700

    ini tugaS 13

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TableController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -10,3 +12,6 @@ Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('regi
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 
 Route::get('/welcome', [AuthController::class, 'showWelcomePage'])->name('welcome');
+
+Route::get('/table', [TableController::class, 'table']);
+Route::get('/data-tables', [TableController::class, 'dataTables']);
